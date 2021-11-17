a0e279e19a928c252985bb8ba43e7112e9825654
Matheus Medeiros <matheusmedeiroscampos@gmail.com>
2021-11-17 18:50:54 -0300

    email unico

--- back/docs/sql/000-user.sql
+++ back/docs/sql/000-user.sql
@@ -1,7 +1,7 @@
 CREATE TABLE user_account (
 	id SERIAL NOT NULL,
 	nickname varchar(80) not null,
-	email varchar(80) not null,
+	email varchar(80) not null unique,
 	password varchar(200) not null,
 	created_at timestamp with time zone not null default CURRENT_TIMESTAMP,
 	updated_at timestamp with time zone default CURRENT_TIMESTAMP,
